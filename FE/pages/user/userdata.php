c66b0e9f65686322ffab4801505a06f7abf57c4c
lukashanzl <lukas.hanzl@icloud.com>
2024-05-02 19:04:22 +0200

    started to implement user data page

--- FE/pages/user/userdata.php
+++ FE/pages/user/userdata.php
@@ -2,7 +2,24 @@
   include_once("./includes/header.php");
 ?>
 
-<p>Here is the user data!</p>
+<div class="user-data-container">
+  <div class="section">
+    <h4>Personal Data</h4>
+    <hr>
+    <input type="text" id="firstname" name="firstname">
+    <label for="firstname">Firstname</label>
+    <input type="text" id="lastname" name="lastname">
+    <label for="lastname">Lastname</label>
+  </div>
+  <div class="section">
+    <h4>Login Information</h4>
+    <hr>
+  </div>
+  <div class="section">
+    <h4>Address Information</h4>
+    <hr>
+  </div>
+</div>
 
 <?php
   include_once("./includes/footer.php");
